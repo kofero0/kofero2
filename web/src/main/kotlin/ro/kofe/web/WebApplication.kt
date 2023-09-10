d4460a5ee330b96f2a21383ed30b99bdac07c079
mitchdrew <mitchdrew@goatse.cx>
2023-09-10 13:30:33 -0700

    open WebApplication

--- web/src/main/kotlin/ro/kofe/web/WebApplication.kt
+++ web/src/main/kotlin/ro/kofe/web/WebApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WebApplication
+open class WebApplication
 
 fun main(args: Array<String>) {
     runApplication<WebApplication>(*args)
