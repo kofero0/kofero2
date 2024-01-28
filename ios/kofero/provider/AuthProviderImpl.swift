630d13f8d86ffb50ab3e95dc5962d3b2919a06bf
mitchdrew <mitchdrew@goatse.cx>
2024-01-28 12:47:21 -0800

    fixed auth provider ios

--- ios/kofero/provider/AuthProviderImpl.swift
+++ ios/kofero/provider/AuthProviderImpl.swift
@@ -27,17 +27,12 @@ class AuthProviderImpl: AuthProvider {
     }
     
     
-    
+    //TODO: FIX, only works with 200s
     private func getRestClosure(dataClosure: @escaping (Data) -> Void) -> RestClosure {
         return {[self] data,response,error in
-            print("HI")
             if let uResponse = response as? HTTPURLResponse {
-                print("HII")
                 if let uData = data {
-                    print("YO")
-                    print(uResponse.statusCode)
                     if(uResponse.statusCode == 200) {
-                        print("NOPE")
                         saveToDisk(data: uData)
                         dataClosure(uData)
                     }
@@ -48,33 +43,31 @@ class AuthProviderImpl: AuthProvider {
     
     
     func get() -> Arrow_coreEither<ModelProviderError, NSString> {
-        print("GETTING AUTH")
         do{
-            print("FINDING AUTH ON DISK")
             //return arrowExtensions.buildImageEitherRight(right: "0||0858cfa32b700b179f75168c19af0160f57aa7de501275fd845d19bae23e7dbe")
             return arrowExtensions.buildImageEitherRight(right: String(data: try Data(contentsOf: makeUrl()), encoding: .utf8)!)
         }
         catch{
-            print("COULDNT FIND IT")
             let group = DispatchGroup()
             var data: Data? = nil
             group.enter()
-            
-            print("IN GROUP")
             var request = URLRequest(url: URL(string: urlPrefix + "/auth/register")!)
             request.httpMethod = "PUT"
             request.allHTTPHeaderFields?["Content-Type"] = "application/json"
             request.httpBody = ("{\"prefixedUid\":\"koferoAuth||" + UIDevice.current.identifierForVendor!.uuidString + "\"}").data(using: .utf8)
-            print(String(data: request.httpBody!, encoding: .utf8))
             restManager.dataTask(with: request, completionHandler: getRestClosure{ [self] ndata in
-                print("HELLO")
                 self.loggingProvider.log(level: .debug, logTag: "AuthProvider", message: "\(String(describing: String(data: ndata, encoding: .utf8)))")
                 data = ndata
                 group.leave()
             }).resume()
             group.wait()
             if let uData = data {
-                return arrowExtensions.buildImageEitherRight(right: String(data: uData, encoding: .utf8)!)
+                do{
+                    let json = try JSON(data: uData)
+                    return arrowExtensions.buildImageEitherRight(right: json["token"].stringValue)
+                }catch{
+                    return arrowExtensions.buildImageEitherLeft(left: ModelIncorrectCount(ids: []))
+                }
             } else{
                 return arrowExtensions.buildImageEitherLeft(left: ModelIncorrectCount(ids: []))
             }
@@ -84,7 +77,8 @@ class AuthProviderImpl: AuthProvider {
     private func saveToDisk(data: Data){
         let path = makeUrl()
         do {
-            try data.write(to: path, options: .atomic)
+            let json = try JSON(data: data)
+            try json["token"].stringValue.data(using: .utf8)?.write(to: path)
         } catch {
             loggingProvider.log(level: .debug, logTag: "AuthProvider", message: error.localizedDescription)
             print(error.localizedDescription)
