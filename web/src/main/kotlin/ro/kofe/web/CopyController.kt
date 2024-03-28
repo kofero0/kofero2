7cca208829271bdd0e2cec0bee29c67d26508334
mitchdrew <mitchdrew@goatse.cx>
2024-03-27 18:50:16 -0700

    fix copy controller mapping

--- web/src/main/kotlin/ro/kofe/web/CopyController.kt
+++ web/src/main/kotlin/ro/kofe/web/CopyController.kt
@@ -3,11 +3,13 @@ package ro.kofe.web
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jsonMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import ro.kofe.model.Copy
 import java.io.InputStream
 
 @RestController
@@ -25,6 +27,6 @@ class CopyController {
 
     @GetMapping(COPY_PATH)
     fun get(): ResponseEntity<Any> {
-        return ResponseEntity<Any>(mapper.writeValueAsString(stream.bufferedReader().readText()), HttpStatus.OK)
+        return ResponseEntity<Any>(mapper.writeValueAsString(mapper.readValue<Copy>(stream.bufferedReader().readText())), HttpStatus.OK)
     }
 }
