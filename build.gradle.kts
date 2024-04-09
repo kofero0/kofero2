5bab6211cbf086cd499804355bee3045eafcd1f4
Mitch Drew <mdrew@Mitchs-MacBook-Pro.local>
2024-04-09 14:57:28 -0700

    android build fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ subprojects {
 
 plugins {
     kotlin("multiplatform") version "1.9.20" apply false
-    id("com.android.application") version "7.4.0" apply false
-    id("com.android.library") version "7.4.0" apply false
+    id("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.3.14" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
